d027eed65b25395851c68fc7de37a23903e77107
hanan <hananmabdirahman@gmail.com>
2023-06-11 01:06:29 -0500

    attempt at title change

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 default_domain = "c"
 extensions = [ "sphinx_rtd_theme" ]
 html_theme = "sphinx_rtd_theme"
-project = "UMN Kernel Dev"
+project = "UMN Kernel Object"
 root_doc = "index"
 source_suffix = ".rst"
