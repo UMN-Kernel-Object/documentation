5389fb1e4506db9675f1eb172a4991ea9df2dc65
Nathan Ringo <nathan@remexre.com>
2024-09-25 14:00:47 -0500

    Typo fixes and "get involved" slide.

--- slides/2024-09-25/project-subgroup/before.c
+++ slides/2024-09-25/project-subgroup/before.c
@@ -1,8 +1,8 @@
 struct stat sb;
 if (stat(path, &sb))
-  perror("stat"), exit(111);
-if(0 /* path is under /project/foo && sb.st_uid is in the foo group
-        && sb.st_gid is the primary group of sb.st_uid */) \{
-  if (chown(path, sb.st_uid, foo_gid))
-    perror("chown"), exit(111);
+        perror("stat"), exit(111);
+if (/* path is under /project/foo && sb.st_uid is in the foo group
+       && sb.st_gid is the primary group of sb.st_uid */) \{
+        if (chown(path, sb.st_uid, foo_gid))
+                perror("chown"), exit(111);
 \}
